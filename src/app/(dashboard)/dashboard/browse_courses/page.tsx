be495f869c8157a45b09127955a61bac4587bac7
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-02-14 14:44:49 +0545

    refactor API endpoints; update user registration and login paths, adjust axios base URL, enhance login and signup hooks with response handling

--- src/app/(dashboard)/dashboard/browse_courses/page.tsx
+++ src/app/(dashboard)/dashboard/browse_courses/page.tsx
@@ -70,3 +70,4 @@ const BrowseCourses = () => {
 };
 
 export default BrowseCourses;
+
--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,36 +1,48 @@
-import { useMutation } from "@tanstack/react-query";
-import { LoginFormInputs } from "@/utils/types";
+import { useMutation, UseMutationResult } from "@tanstack/react-query";
+import { LoginFormInputs, loginResponse } from "@/utils/types";
 import { Endpoints } from "@/api/endpoints";
 import axiosInstance from "@/axiosinstance/axiosInstance";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { addToast } from "@/redux/slices/toastSlice";
-const {userlogin} = Endpoints;
-const login = async (loginData:LoginFormInputs):Promise<LoginFormInputs | undefined> => {
-    try{
-const response = await axiosInstance.post(userlogin,loginData);
-return response.data as LoginFormInputs;
-    }catch(error:unknown){
-        if(error instanceof Error){
-            console.error(error.message)
-        }else{
-            throw new Error("An unknown error occurred. Please try again later.");
-        }
+import Cookies from "js-cookie";
+const { userlogin } = Endpoints;
+
+const login = async (loginData: LoginFormInputs): Promise<loginResponse> => {
+  try {
+    const response = await axiosInstance.post<loginResponse>(userlogin, loginData);
+    return response.data;
+  } catch (error: unknown) {
+  
+    if (error instanceof Error) {
+      console.error(`Login failed: ${error.message}`);
+      throw new Error(error.message); 
     }
-}
 
+    throw new Error("An unknown error occurred. Please try again later.");
+  }
+};
 
+export const UseLogin = (): UseMutationResult<loginResponse, Error, LoginFormInputs> => {
+  const dispatch = useDispatch();
 
-export const UseLogin = ()=>{
-const dispatch = useDispatch();
-    return useMutation({
-        mutationKey:['login'],
-        mutationFn:login,
-        onSuccess:()=>{
-            dispatch(addToast({message:"Login Successful",type:'success'}));
+  return useMutation<loginResponse, Error, LoginFormInputs>({
+    mutationKey: ["login"],
+    mutationFn: login,
+    onSuccess: (data) => {
+      const token = data?.token;
 
-        },
-        onError:()=>{
-dispatch(addToast({message:'Login Failed!',type:'error'}));
-        }
-    });
-}
+      if (token) {
+        Cookies.set("token", token, {
+          expires: 7,
+          secure: true,
+          sameSite: "Strict",
+        });
+        dispatch(addToast({ message: "Login Successful", type: "success" }));
+      }
+    },
+    onError: (error: Error) => {
+      console.error(`Error during login: ${error.message}`);
+      dispatch(addToast({ message: "Login Failed!", type: "error" }));
+    },
+  });
+};
