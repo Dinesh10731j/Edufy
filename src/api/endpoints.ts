7a085ebb811fd7c8d994e972cd4e3a70efa9a3d6
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-02-16 12:19:27 +0545

    add live streaming feature; implement start and end stream functionality, integrate socket.io for real-time chat and viewer updates

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,5 +1,6 @@
 export const Endpoints ={
     register:'/api/v1/user/signup',
     userlogin:'/api/v1/user/login',
-    userContact:'/users/contact'
+    userContact:'/users/contact',
+    startLive:'/api/v1/livestream/start'
 }
